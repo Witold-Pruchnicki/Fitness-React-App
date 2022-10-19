108a393a7cd9f033d29c1afa3c103a838b995364
Witold Pruchnicki <witekpruchnicki@gmail.com>
2022-10-19 16:07:59 +0200

    basic funcionalities and design on homepage done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,18 @@ import Exercises from "../components/Exercises";
 import SearchExercises from "../components/SearchExercises";
 import HeroBanner from "../components/HeroBanner";
 import {Box} from "@mui/material";
+import {useState} from "react";
 
-const Home = ()=>{
+
+const Home = () => {
+    const [bodyPart, setBodyPart] = useState('all')
+    const [exercises, setExercises] = useState([])
+
     return (
         <Box>
             <HeroBanner/>
-            <SearchExercises/>
-            <Exercises />
+            <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+            <Exercises setExercises={setExercises} bodyPart={bodyPart} exercises={exercises}/>
         </Box>
     )
 }
